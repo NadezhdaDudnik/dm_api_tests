86475b79b8982a632463eccb6315fa97b815c852
nadin869 <nady86.86@mail.ru>
2023-08-18 01:00:30 +0300

    добавлен тест на проверку поиска

---
+++ tests/test_search/test_search.py
@@ -0,0 +1,24 @@
+import pytest
+
+from dm_api_search_async import SearchRequest, SearchEntityType
+
+
+def test_search(grpc_search):
+    response = grpc_search.search(
+        query='test_nadin',
+        size=10,
+        skip=0,
+        search_across=['FORUM_TOPIC']
+    )
+
+@pytest.mark.asyncio
+async def test_search_async(grpc_search_async):
+    response = await grpc_search_async.search(
+        search_request=SearchRequest(
+            query='test_nadin',
+            size=10,
+            skip=0,
+            search_across=[SearchEntityType.FORUM_TOPIC]
+        )
+    )
+    print(response)
